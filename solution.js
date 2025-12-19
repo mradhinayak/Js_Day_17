8524c4b58d4df25f2b4b404cea1387e5d3175d14
Shivam Goswami <mradhinayak@gmail.com>
2025-12-19 19:59:55 +0530

    Print Numbers from N to 1 Using Recursion

--- solution.js
+++ solution.js
@@ -1,5 +1,16 @@
+// Print Numbers from 1 to N Using Recursion;
 function printNumbers(num) {
     if (num > 1) printNumbers(num - 1);
     console.log(num);
 }
 printNumbers(3)
+
+
+
+// Print Numbers from N to 1 Using Recursion;
+function printNumbersReverse(num) {
+     console.log(num);
+    if (num > 1) printNumbersReverse(num - 1);
+   
+}
+printNumbersReverse(5)
